f8f42aa1bb8b9a19277a53ce4808724d6432dea6
Ondrej Slinták <ondrowan@gmail.com>
2019-07-31 17:32:57 +0200

    Add function for calculating of checksum

--- src/file.rs
+++ src/file.rs
@@ -1,5 +1,9 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 use std::path::Path;
 
+use crc32fast;
 use tobj;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -16,4 +20,19 @@ pub fn load_obj(file_path: &str) -> tobj::LoadResult {
     let path = Path::new(file_path);
 
     tobj::load_obj(&path)
+}
+
+pub fn calculate_checksum(file_path: &str) -> u32 {
+    let file = File::open(file_path).unwrap_or_else(|_| panic!("File {} to be loaded", file_path));
+    let mut reader = BufReader::new(file);
+    let mut contents = String::new();
+    reader
+        .read_to_string(&mut contents)
+        .unwrap_or_else(|_| panic!("Read contents of file {} into string.", file_path));
+
+    let bytes = contents.into_bytes();
+    let mut hasher = crc32fast::Hasher::new();
+
+    hasher.update(&bytes);
+    hasher.finalize()
 }
