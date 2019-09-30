c809aba85dca26123f36bcaa066c23300fac82ba
janper <daroiz@janper.sk>
2019-09-30 06:22:15 +0200

    find border edges and half-edges

--- src/mesh_analysis.rs
+++ src/mesh_analysis.rs
@@ -1,6 +1,6 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
-use crate::geometry::Geometry;
+use crate::geometry::{Edge, Geometry, HalfEdge};
 
 /// Check if all the vertices of geometry are referenced in geometry's faces
 #[allow(dead_code)]
@@ -12,4 +12,20 @@ pub fn has_no_orphans(geo: &Geometry) -> bool {
         used_vertices.insert(face.vertices.2);
     }
     used_vertices.len() == geo.vertices().len()
+}
+
+/// Finds border edges in a mesh edge collection
+/// An edge is border when its valency is 1
+#[allow(dead_code)]
+pub fn border_edges(edges: HashMap<Edge, usize>) -> Vec<Edge> {
+    let keys = edges.keys();
+    keys.filter(|key| edges[key] == 1).copied().collect()
+}
+
+/// Finds border edges in a mesh half-edge collection
+/// A half-edge is border when its valency is 1
+#[allow(dead_code)]
+pub fn border_half_edges(half_edges: HashMap<HalfEdge, usize>) -> Vec<HalfEdge> {
+    let keys = half_edges.keys();
+    keys.filter(|key| half_edges[key] == 1).copied().collect()
 }
