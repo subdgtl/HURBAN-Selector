8d558441ecdbf81373de71f3accc8627ab6378e6
janper <daroiz@janper.sk>
2019-09-30 06:45:50 +0200

    find border vertex indices

--- src/mesh_analysis.rs
+++ src/mesh_analysis.rs
@@ -17,15 +17,49 @@ pub fn has_no_orphans(geo: &Geometry) -> bool {
 /// Finds border edges in a mesh edge collection
 /// An edge is border when its valency is 1
 #[allow(dead_code)]
-pub fn border_edges(edges: HashMap<Edge, usize>) -> Vec<Edge> {
-    let keys = edges.keys();
-    keys.filter(|key| edges[key] == 1).copied().collect()
+pub fn border_edges(edge_valencies: HashMap<Edge, usize>) -> Vec<Edge> {
+    let keys = edge_valencies.keys();
+    keys.filter(|key| edge_valencies[key] == 1)
+        .copied()
+        .collect()
 }
 
 /// Finds border edges in a mesh half-edge collection
 /// A half-edge is border when its valency is 1
 #[allow(dead_code)]
-pub fn border_half_edges(half_edges: HashMap<HalfEdge, usize>) -> Vec<HalfEdge> {
-    let keys = half_edges.keys();
-    keys.filter(|key| half_edges[key] == 1).copied().collect()
+pub fn border_half_edges(half_edge_valencies: HashMap<HalfEdge, usize>) -> Vec<HalfEdge> {
+    let keys = half_edge_valencies.keys();
+    keys.filter(|key| half_edge_valencies[key] == 1)
+        .copied()
+        .collect()
+}
+
+/// Finds border vertex indices in a mesh half-edge collection
+/// A vertex is border when its half-edge's valency is 1
+#[allow(dead_code)]
+pub fn border_vertex_indices_from_half_edges(
+    half_edge_valencies: HashMap<HalfEdge, usize>,
+) -> Vec<u32> {
+    let keys = half_edge_valencies.keys();
+    let mut border_vertices = HashSet::new();
+    keys.filter(|key| half_edge_valencies[key] == 1)
+        .for_each(|half_edge| {
+            border_vertices.insert(half_edge.vertices.0);
+            border_vertices.insert(half_edge.vertices.1);
+        });
+    border_vertices.iter().copied().collect()
+}
+
+/// Finds border vertex indices in a mesh edge collection
+/// A vertex is border when its edge's valency is 1
+#[allow(dead_code)]
+pub fn border_vertex_indices_from_edges(edge_valencies: HashMap<Edge, usize>) -> Vec<u32> {
+    let keys = edge_valencies.keys();
+    let mut border_vertices = HashSet::new();
+    keys.filter(|key| edge_valencies[key] == 1)
+        .for_each(|edge| {
+            border_vertices.insert(edge.vertices.0);
+            border_vertices.insert(edge.vertices.1);
+        });
+    border_vertices.iter().copied().collect()
 }
