fc51bc9c75fcc9c61ffa89610a3e11f8ac7962f3
janper <daroiz@janper.sk>
2019-09-30 08:02:19 +0200

    removed functions specific for OrientedEdge and UnorientedEdge

--- src/edge_analysis.rs
+++ src/edge_analysis.rs
@@ -1,7 +1,7 @@
 use std::cmp;
 use std::collections::HashMap;
 
-use crate::geometry::{Edge, OrientedEdge, UnorientedEdge};
+use crate::geometry::{Edge, OrientedEdge};
 
 /// Calculate edge valencies = number of faces sharing an edge
 /// 1 -> border edge = acceptable but mesh is not watertight
@@ -39,48 +39,4 @@ pub fn edge_valencies(edges: &[Edge]) -> HashMap<Edge, usize> {
         }
     }
     edge_valency_map
-}
-
-/// Calculate unoriented edge valencies = number of faces sharing an unoriented edge
-/// 1 -> border unoriented edge = acceptable but mesh is not watertight
-/// 2 -> manifold unoriented edge = correct
-/// 3 or more -> non-manifold unoriented edge = corrupted mesh
-#[allow(dead_code)]
-pub fn unoriented_edge_valencies(
-    unoriented_edges: &[UnorientedEdge],
-) -> HashMap<UnorientedEdge, usize> {
-    let mut unoriented_edge_valency_map: HashMap<UnorientedEdge, usize> = HashMap::new();
-    for unoriented_edge in unoriented_edges {
-        let valencies = match unoriented_edge_valency_map.get(unoriented_edge) {
-            Some(v) => *v + 1,
-            None => 1,
-        };
-        unoriented_edge_valency_map.insert(*unoriented_edge, valencies);
-    }
-    unoriented_edge_valency_map
-}
-
-/// Calculate oriented edge valencies = number of faces sharing a oriented edge and its reversed oriented edge
-/// 1 -> border oriented edge = acceptable but mesh is not watertight
-/// 2 -> manifold oriented edge = correct
-/// 3 or more -> non-manifold oriented edge = corrupted mesh
-#[allow(dead_code)]
-pub fn oriented_edge_valencies(oriented_edges: &[OrientedEdge]) -> HashMap<OrientedEdge, usize> {
-    let mut oriented_edge_valency_map: HashMap<OrientedEdge, usize> = HashMap::new();
-    for oriented_edge in oriented_edges {
-        let one_way_valencies = match oriented_edge_valency_map.get(oriented_edge) {
-            Some(v) => *v + 1,
-            None => 1,
-        };
-        let other_oriented_edge =
-            OrientedEdge::from((oriented_edge.vertices.1, oriented_edge.vertices.0));
-        let other_way_valencies = match oriented_edge_valency_map.get(&other_oriented_edge) {
-            Some(v) => *v + 1,
-            None => 0,
-        };
-        let valencies = cmp::max(one_way_valencies, other_way_valencies);
-        oriented_edge_valency_map.insert(*oriented_edge, valencies);
-        oriented_edge_valency_map.insert(other_oriented_edge, valencies);
-    }
-    oriented_edge_valency_map
 }
--- src/mesh_analysis.rs
+++ src/mesh_analysis.rs
@@ -1,6 +1,6 @@
 use std::collections::{HashMap, HashSet};
 
-use crate::geometry::{Edge, Geometry, OrientedEdge, UnorientedEdge};
+use crate::geometry::{Edge, Geometry};
 
 /// Check if all the vertices of geometry are referenced in geometry's faces
 #[allow(dead_code)]
@@ -24,30 +24,6 @@ pub fn border_edges(edge_valencies: HashMap<Edge, usize>) -> Vec<Edge> {
         .collect()
 }
 
-/// Finds border edges in a mesh unoriented edge collection
-/// An unoriented edge is border when its valency is 1
-#[allow(dead_code)]
-pub fn border_unoriented_edges(
-    unoriented_edge_valencies: HashMap<UnorientedEdge, usize>,
-) -> Vec<UnorientedEdge> {
-    let keys = unoriented_edge_valencies.keys();
-    keys.filter(|key| unoriented_edge_valencies[key] == 1)
-        .copied()
-        .collect()
-}
-
-/// Finds border edges in a mesh oriented edge collection
-/// A oriented edge is border when its valency is 1
-#[allow(dead_code)]
-pub fn border_oriented_edges(
-    oriented_edge_valencies: HashMap<OrientedEdge, usize>,
-) -> Vec<OrientedEdge> {
-    let keys = oriented_edge_valencies.keys();
-    keys.filter(|key| oriented_edge_valencies[key] == 1)
-        .copied()
-        .collect()
-}
-
 /// Finds border vertex indices in a mesh edge collection
 /// A vertex is border when its edge's valency is 1
 #[allow(dead_code)]
@@ -62,38 +38,6 @@ pub fn border_vertex_indices_from_edges(edge_valencies: HashMap<Edge, usize>) ->
             };
             border_vertices.insert(vertices.0);
             border_vertices.insert(vertices.1);
-        });
-    border_vertices.iter().copied().collect()
-}
-
-/// Finds border vertex indices in a mesh oriented edge collection
-/// A vertex is border when its oriented edge's valency is 1
-#[allow(dead_code)]
-pub fn border_vertex_indices_from_oriented_edges(
-    oriented_edge_valencies: HashMap<OrientedEdge, usize>,
-) -> Vec<u32> {
-    let keys = oriented_edge_valencies.keys();
-    let mut border_vertices = HashSet::new();
-    keys.filter(|key| oriented_edge_valencies[key] == 1)
-        .for_each(|oriented_edge| {
-            border_vertices.insert(oriented_edge.vertices.0);
-            border_vertices.insert(oriented_edge.vertices.1);
-        });
-    border_vertices.iter().copied().collect()
-}
-
-/// Finds border vertex indices in a mesh unoriented edge collection
-/// A vertex is border when its unoriented edge's valency is 1
-#[allow(dead_code)]
-pub fn border_vertex_indices_from_unoriented_edges(
-    unoriented_edge_valencies: HashMap<UnorientedEdge, usize>,
-) -> Vec<u32> {
-    let keys = unoriented_edge_valencies.keys();
-    let mut border_vertices = HashSet::new();
-    keys.filter(|key| unoriented_edge_valencies[key] == 1)
-        .for_each(|unoriented_edge| {
-            border_vertices.insert(unoriented_edge.vertices.0);
-            border_vertices.insert(unoriented_edge.vertices.1);
         });
     border_vertices.iter().copied().collect()
 }
