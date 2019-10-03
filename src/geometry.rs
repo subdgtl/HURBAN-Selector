d54a008572f540b97cef45b33363026abbb2c488
janper <daroiz@janper.sk>
2019-10-03 23:10:43 +0200

    test for orphan vertex search passing

--- src/geometry.rs
+++ src/geometry.rs
@@ -787,4 +787,25 @@ mod tests {
             normals.clone(),
         );
     }
+
+    #[test]
+    fn test_orphan_vertex_detection() {
+        let (faces, vertices, normals) = quad_with_normals();
+        let extra_vertex = vec![v(0.0, 0.0, 0.0, [0.0, 0.0, 0.0], 1.0)];
+        let vertices_extended = [&vertices[..], &extra_vertex[..]].concat();
+        let geometry_with_orphans = Geometry::from_triangle_faces_with_vertices_and_normals(
+            faces.clone(),
+            vertices_extended.clone(),
+            normals.clone(),
+        );
+
+        let geometry_without_orphans = Geometry::from_triangle_faces_with_vertices_and_normals(
+            faces.clone(),
+            vertices.clone(),
+            normals.clone(),
+        );
+
+        assert!(geometry_without_orphans.has_no_orphans());
+        assert!(!geometry_with_orphans.has_no_orphans());
+    }
 }
