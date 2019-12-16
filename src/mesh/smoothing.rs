ca4bd44fccd10ef95b1bdc681c63b9a4c09b9521
janper <janper@janper.sk>
2019-12-16 01:19:18 +0100

    Add expects to loop smoothing

--- src/mesh/smoothing.rs
+++ src/mesh/smoothing.rs
@@ -318,9 +318,12 @@ pub fn loop_subdivision(
                     mid_vertex_indices[edge_index] = Some(mid_vertex_index);
                 }
 
-                let mid_v1v2_index = mid_vertex_indices[0]?;
-                let mid_v2v3_index = mid_vertex_indices[1]?;
-                let mid_v3v1_index = mid_vertex_indices[2]?;
+                let mid_v1v2_index =
+                    mid_vertex_indices[0].expect("Must have been produced by earlier loop");
+                let mid_v2v3_index =
+                    mid_vertex_indices[1].expect("Must have been produced by earlier loop");
+                let mid_v3v1_index =
+                    mid_vertex_indices[2].expect("Must have been produced by earlier loop");
 
                 faces.push((vi1, mid_v1v2_index, mid_v3v1_index));
                 faces.push((vi2, mid_v2v3_index, mid_v1v2_index));
