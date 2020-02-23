b4f8e6a2983ef147e71e4ed3ceff20e39b3570df
janper <janper@janper.sk>
2020-02-23 17:36:00 +0100

    Set demo project to voxel morph

--- src/demo.rs
+++ src/demo.rs
@@ -1,5 +1,8 @@
 use crate::interpreter::ast;
-use crate::interpreter_funcs::{FUNC_ID_CREATE_BOX, FUNC_ID_CREATE_UV_SPHERE};
+use crate::interpreter_funcs::{
+    FUNC_ID_ALIGN, FUNC_ID_IMPORT_OBJ_JOIN, FUNC_ID_INTERPOLATED_UNION,
+    FUNC_ID_LAPLACIAN_SMOOTHING, FUNC_ID_TRANSFORM,
+};
 use crate::project::Project;
 
 pub fn default_project() -> Project {
@@ -9,26 +12,79 @@ pub fn default_project() -> Project {
             ast::Stmt::VarDecl(ast::VarDeclStmt::new(
                 ast::VarIdent(0),
                 ast::CallExpr::new(
-                    FUNC_ID_CREATE_BOX,
+                    FUNC_ID_IMPORT_OBJ_JOIN,
                     vec![
-                        ast::Expr::Lit(ast::LitExpr::Float3([-1.0, 0.0, 1.0])),
-                        ast::Expr::Lit(ast::LitExpr::Float3([0.0, 0.0, 0.0])),
-                        ast::Expr::Lit(ast::LitExpr::Float3([1.0, 1.0, 1.0])),
+                        ast::Expr::Lit(ast::LitExpr::String("c:\\tatra.obj".to_string())),
+                        ast::Expr::Lit(ast::LitExpr::Boolean(false)),
+                        ast::Expr::Lit(ast::LitExpr::Boolean(false)),
+                        ast::Expr::Lit(ast::LitExpr::Boolean(true)),
+                        ast::Expr::Lit(ast::LitExpr::Boolean(false)),
+                        ],
+                    ),
+                )),
+                ast::Stmt::VarDecl(ast::VarDeclStmt::new(
+                    ast::VarIdent(1),
+                    ast::CallExpr::new(
+                        FUNC_ID_IMPORT_OBJ_JOIN,
+                        vec![
+                            ast::Expr::Lit(ast::LitExpr::String("c:\\auticko.obj".to_string())),
+                        ast::Expr::Lit(ast::LitExpr::Boolean(false)),
+                        ast::Expr::Lit(ast::LitExpr::Boolean(false)),
                         ast::Expr::Lit(ast::LitExpr::Boolean(true)),
                         ast::Expr::Lit(ast::LitExpr::Boolean(false)),
                     ],
                 ),
             )),
             ast::Stmt::VarDecl(ast::VarDeclStmt::new(
-                ast::VarIdent(1),
+                ast::VarIdent(2),
                 ast::CallExpr::new(
-                    FUNC_ID_CREATE_UV_SPHERE,
+                    FUNC_ID_ALIGN,
                     vec![
-                        ast::Expr::Lit(ast::LitExpr::Float3([1.0, 0.0, 1.0])),
+                        ast::Expr::Var(ast::VarExpr::new(ast::VarIdent(1))),
+                        ast::Expr::Var(ast::VarExpr::new(ast::VarIdent(0))),
+                        ast::Expr::Lit(ast::LitExpr::Boolean(true)),
+                        ast::Expr::Lit(ast::LitExpr::Boolean(false)),
+                    ],
+                ),
+            )),
+            ast::Stmt::VarDecl(ast::VarDeclStmt::new(
+                ast::VarIdent(3),
+                ast::CallExpr::new(
+                    FUNC_ID_TRANSFORM,
+                    vec![
+                        ast::Expr::Var(ast::VarExpr::new(ast::VarIdent(2))),
+                        ast::Expr::Lit(ast::LitExpr::Float3([0.0, -21.0, 0.0])),
                         ast::Expr::Lit(ast::LitExpr::Float3([0.0, 0.0, 0.0])),
                         ast::Expr::Lit(ast::LitExpr::Float3([1.0, 1.0, 1.0])),
-                        ast::Expr::Lit(ast::LitExpr::Uint(16)),
-                        ast::Expr::Lit(ast::LitExpr::Uint(16)),
+                        ast::Expr::Lit(ast::LitExpr::Boolean(true)),
+                        ast::Expr::Lit(ast::LitExpr::Boolean(true)),
+                        ast::Expr::Lit(ast::LitExpr::Boolean(false)),
+                    ],
+                ),
+            )),
+            ast::Stmt::VarDecl(ast::VarDeclStmt::new(
+                ast::VarIdent(4),
+                ast::CallExpr::new(
+                    FUNC_ID_INTERPOLATED_UNION,
+                    vec![
+                        ast::Expr::Var(ast::VarExpr::new(ast::VarIdent(0))),
+                        ast::Expr::Var(ast::VarExpr::new(ast::VarIdent(3))),
+                        ast::Expr::Lit(ast::LitExpr::Float3([10.0, 10.0, 10.0])),
+                        ast::Expr::Lit(ast::LitExpr::Boolean(true)),
+                        ast::Expr::Lit(ast::LitExpr::Float(0.5)),
+                        ast::Expr::Lit(ast::LitExpr::Boolean(true)),
+                        ast::Expr::Lit(ast::LitExpr::Boolean(true)),
+                        ast::Expr::Lit(ast::LitExpr::Boolean(false)),
+                    ],
+                ),
+            )),
+            ast::Stmt::VarDecl(ast::VarDeclStmt::new(
+                ast::VarIdent(5),
+                ast::CallExpr::new(
+                    FUNC_ID_LAPLACIAN_SMOOTHING,
+                    vec![
+                        ast::Expr::Var(ast::VarExpr::new(ast::VarIdent(4))),
+                        ast::Expr::Lit(ast::LitExpr::Uint(4)),
                         ast::Expr::Lit(ast::LitExpr::Boolean(true)),
                         ast::Expr::Lit(ast::LitExpr::Boolean(true)),
                         ast::Expr::Lit(ast::LitExpr::Boolean(false)),
